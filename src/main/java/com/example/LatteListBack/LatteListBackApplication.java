38da0becda20e3bdf1cda1e28dc4f6931fc8b8fa
BlueAgus <240250687+agustinamassuco-crypto@users.noreply.github.com>
2025-11-22 18:31:13 -0300

    Initial commit
    Proyecto LatteList Backend base

---
+++ src/main/java/com/example/LatteListBack/LatteListBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.LatteListBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteListBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatteListBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LatteListBack/LatteListBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LatteListBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteListBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
